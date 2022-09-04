a2ca78e9fd96b4fa532f3e93b0d1afd629001275
Lakshmi Narasimman <lakshminarasimman.v17@gmail.com>
2022-09-04 19:50:08 +0530

    updated terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -2,11 +2,10 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.29.0"
+      version = ">= 4.29.0"
     }
   }
 }
 
 provider "aws" {
-  region = "ap-southeast-1"
 }
